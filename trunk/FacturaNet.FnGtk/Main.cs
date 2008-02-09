2ccb8b89420ba5dbb98b2ea3c5a290647e7ad495
andresmoschini <andresmoschini@44542eff-9745-0410-9149-3502c1885c35>
2008-02-09 00:54:48 +0000

    Había perdido el Main

---
+++ trunk/FacturaNet.FnGtk/Main.cs
@@ -0,0 +1,58 @@
+// project created on 06/02/2008 at 19:43
+using System;
+using Gtk;
+using FacturaNet.FnNegocio;
+using AmUtil;
+
+namespace FacturaNet.FnGtk
+{
+	class MainClass
+	{
+		public static bool CmdMode(string[] args)
+		{
+			bool cmdMode = false;
+			if (args.Length > 0)
+			{
+				switch (args[0])
+				{
+					case "--crear_usuario" : 
+						Sesion.SesionSingleton.CrearUsuario(args[1],args[2]);
+						Console.WriteLine("Usuario creado exitosamente");
+						cmdMode = true;
+						break;
+					case "--help" :
+						Console.WriteLine("AYUDA");
+						cmdMode = true;
+						break;
+					default :
+						Console.WriteLine("Parametro desconocido, se continua con el modo gráfico");
+						break;
+				}
+			}
+			return cmdMode;
+		}
+		public static void Main (string[] args)
+		{
+			if (!CmdMode(args))
+			{
+				Application.Init ();
+			
+				FrmLogin loginWindow = new FrmLogin();
+				loginWindow.Show();
+				
+				Application.Run();			
+				loginWindow.Destroy();
+				
+				if (Sesion.SesionSingleton.RolDb == RolDb.UsuarioStandard)
+				{
+					FrmPrincipal frmPrincipal = new FrmPrincipal();
+					frmPrincipal.Show ();
+					Application.Run ();
+				}
+				else if (Sesion.SesionSingleton.RolDb == RolDb.NoEsUsuario)
+					Util.Log("El par usuario,clave no es válido."); 
+			}
+			Util.Log("SALE");
+		}
+	}
+}
