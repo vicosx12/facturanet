9ef0e9a610d53f9fb8c200c852a373fb77842081
andresmoschini <andresmoschini@44542eff-9745-0410-9149-3502c1885c35>
2008-02-19 00:21:48 +0000

    Ahora es independiente de la base de datos

--- trunk/FacturaNet.FnNegocio/Sesion.cs
+++ trunk/FacturaNet.FnNegocio/Sesion.cs
@@ -6,46 +6,67 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Data.Common;
-using FirebirdSql.Data.FirebirdClient;
-using MySql.Data.MySqlClient;
 using AmUtil;
 
 namespace FacturaNet.FnNegocio
 {
 	public class Sesion
 	{
-		static private string providerName {get{return GetSesionCfg("ProviderName");}}
-		
-		static private string realUser {get{return GetSesionCfg("RealUser");}}
-		static private string realPassword {get{return GetSesionCfg("RealPassword");}}
-		static private string server {get{return GetSesionCfg("Server");}}
-		static private string dataBase {get{return GetSesionCfg("DataBase");}}
-		static private string cnnString {get{return GetSesionCfg("CnnString");}} 
-		static private DbProviderFactory _dbpFactory 
-		{
-			get
-			{
-				Console.WriteLine();Console.WriteLine();
-				foreach (DataRow row in DbProviderFactories.GetFactoryClasses().Rows)
-					Console.WriteLine("{0}\t{1}\t{2}\t{3}\n",row[0], row[1], row[2], row[3]);
-				Console.WriteLine();Console.WriteLine();
-				
-				return DbProviderFactories.GetFactory(providerName);
-			}
-		}
-
+        #region configuracion
+		static private Configuration config = null;
 		static private string GetSesionCfg(string configuracion)
 		{
-			System.Configuration.Configuration config =
-				ConfigurationManager.OpenExeConfiguration(
-				      ConfigurationUserLevel.None);
+			if (config == null) 
+				config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 			//config.AppSettings.Settings.Add("a", DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString());
 			//config.Save(ConfigurationSaveMode.Modified);
 			return config.AppSettings.Settings[configuracion].Value;			
 		}
-
-		static private Sesion sesion = null;
+		static private string providerName 
+		{
+			get { return GetSesionCfg("ProviderName"); } 
+		}
+		static private string realUser
+		{
+			get { return GetSesionCfg("RealUser"); }
+		}
+		static private string realPassword 
+		{
+			get { return GetSesionCfg("RealPassword"); }
+		}
+		static private string server 
+		{
+			get { return GetSesionCfg("Server"); }
+		}
+		static private string dataBase 
+		{
+			get { return GetSesionCfg("DataBase"); }
+		}
+		static private string cnnString 
+		{
+			get { return GetSesionCfg("CnnString"); }
+		} 
+        #endregion
 		
+		
+		static private DbProviderFactory _dbpFactory = null;
+		static private DbProviderFactory dbpFactory 
+		{
+			get
+			{
+				if (_dbpFactory == null)
+				{
+					Console.WriteLine(); Console.WriteLine();
+					foreach (DataRow row in DbProviderFactories.GetFactoryClasses().Rows)
+						Console.WriteLine("{0}\t{1}\t{2}\t{3}\n",row[0], row[1], row[2], row[3]);
+					Console.WriteLine(); Console.WriteLine();
+					_dbpFactory = DbProviderFactories.GetFactory(providerName);
+				}
+				return _dbpFactory;
+			}
+		}
+		
+		static private Sesion sesion = null;		
 		static public Sesion SesionSingleton
 		{	
 			get
@@ -56,98 +77,96 @@ namespace FacturaNet.FnNegocio
 			}
 		}
 
-		static public int FillUsuarios(DataTable tablaUsuarios)
+		public int FillUsuarios(DataTable tablaUsuarios)
 		{
-			FbDataAdapter dataAdapter = new FbDataAdapter(
+			DbDataAdapter da = CreateDataAdapter(
 @"
 SELECT  
 	NB_USUARIO,
 	DES_USUARIO
 FROM
-	SPS_LST_USUARIOS",
-			                                              _GetFbConnection());
-			return dataAdapter.Fill(tablaUsuarios);
+	SPS_LST_USUARIOS");
+			return da.Fill(tablaUsuarios);
 		}
 		
 		
 		private string user = "";
 		private string password = "";
-		//private RolDb rolDb = RolDb.NoAsignado;
 		private bool conectado = false;
 
 		private Sesion()
 		{
 		}
-		
-		/*
-		private string StrRol
-		{
-			get 
-			{
-				// TODO: definir los roles en la db para que no pueda hacer cualquier cosa
-				switch (rolDb)
-				{
-				case RolDb.NoAsignado : 
-					return "";
-					//return "Role=ROL_SINASIGNAR";
-				case RolDb.UsuarioStandard :
-					return ""; 					 
-					//return "Role=ROL_USUARIO;";
-				default :
-					return "";//"Role=ROL_SINPERMISOS;";
-				}
-			}
-		}
-		*/
-		/*	
-		static private FbConnection _GetFbConnection(string strRol)
-		{
-			return new FbConnection(
-				                        "User=" + realUser + ";" +
-				                        "Password=" + realPassword + ";" +
-				                        "Database=" + dataBase + ";" +
-				                        "DataSource=" + server + ";" +
-				                        strRol);
-		}
-		*/
-		
-		static private FbConnection _GetFbConnection()
-		{
-			return new FbConnection(
-			                        string.Format(
-			                                      cnnString,
-			                                      server,
-			                                      dataBase,
-			                                      realUser,
-			                                      realPassword));				                       
-		}
-
-/*		public RolDb RolDb
-		{
-			get { return this.rolDb; }				
-		}
-*/
 		public bool Conectado
 		{
 			get {return conectado;}
 		}
-		public FbConnection GetFbConnection()
+		public DbConnection CreateConnection()
 		{
-			return _GetFbConnection();
+			DbConnection cnn = dbpFactory.CreateConnection();
+			cnn.ConnectionString = string.Format(
+			                                      cnnString,
+			                                      server,
+			                                      dataBase,
+			                                      realUser,
+			                                      realPassword);
+			return cnn;
 		}
-
+		public DbDataAdapter CreateDataAdapter()
+		{
+			return dbpFactory.CreateDataAdapter();
+		}
+		public DbDataAdapter CreateDataAdapter(string selectCommand)
+		{
+			DbDataAdapter da = CreateDataAdapter();
+			DbCommand cmd = CreateCommand(selectCommand);
+			da.SelectCommand = cmd;
+			return da;
+		}
+		public DbCommand CreateCommand()
+		{
+			DbCommand cmd = dbpFactory.CreateCommand();
+			cmd.Connection = CreateConnection();
+			return cmd;
+		}
+		public DbCommand CreateCommand(string commandText)
+		{
+			DbCommand cmd = CreateCommand();
+			cmd.CommandText = commandText;
+			return cmd;
+		}
+		public DbParameter CreateParameter()
+		{
+			return dbpFactory.CreateParameter();
+		}
+		public DbParameter CreateParameter(string parameterName, DbType dbType)
+		{
+			DbParameter par = CreateParameter();
+			par.ParameterName = parameterName;
+			par.DbType = dbType;
+			return par;
+		}
+		public DbParameter CreateParameter(string parameterName, DbType dbType, object value)
+		{
+			DbParameter par = CreateParameter(parameterName, dbType);
+			par.Value = value;
+			return par;
+		}
 		public bool ReConectar()
 		{
 			conectado = false;
-			FbCommand cmd = new FbCommand();
-			cmd.Connection = GetFbConnection();
+			DbCommand cmd = CreateCommand("SPS_VRF_USUARIO"); 
 			cmd.CommandType = CommandType.StoredProcedure;
-			cmd.Parameters.Add("@NB_USUARIO",user);
-			cmd.Parameters.Add("@CLAVE",Util.CalcularSHA1(password));
-			cmd.CommandText = "SPS_VRF_USUARIO";
+			cmd.Parameters.Add(CreateParameter(
+			                                   "@NB_USUARIO",
+			                                   DbType.String,
+			                                   user));  
+			cmd.Parameters.Add(CreateParameter(
+			                                   "@CLAVE",
+			                                   DbType.String,
+			                                   Util.CalcularSHA1(password)));  
 			cmd.Connection.Open();
 			conectado = (int)cmd.ExecuteScalar() == 1;
-			//rolDb = (RolDb)(int)cmd.ExecuteScalar();
 			cmd.Connection.Close();
 			if (conectado) 
 				Util.Log("Usuario autentificado"); 
@@ -172,13 +191,20 @@ FROM
 
 		public void CrearUsuario(string user, string password)
 		{
-			FbCommand cmd = new FbCommand();
-			cmd.Connection = GetFbConnection();
+			DbCommand cmd = CreateCommand("SPS_NEW_USUARIO");
 			cmd.CommandType = CommandType.StoredProcedure;
-			cmd.CommandText = "SPS_NEW_USUARIO";
-			cmd.Parameters.Add("@NB_USUARIO",user);
-			cmd.Parameters.Add("@DES_USUARIO",user);
-			cmd.Parameters.Add("@CLAVE",Util.CalcularSHA1(password));
+			cmd.Parameters.Add(CreateParameter(
+			                                   "@NB_USUARIO",
+			                                   DbType.String,
+			                                   user));
+			cmd.Parameters.Add(CreateParameter(
+			                                   "@DES_USUARIO",
+			                                   DbType.String,
+			                                   user));
+			cmd.Parameters.Add(CreateParameter(
+			                                   "@CLAVE",
+			                                   DbType.String,
+			                                   Util.CalcularSHA1(password)));  
 			cmd.Connection.Open();
 			cmd.ExecuteNonQuery();
 			cmd.Connection.Close();
